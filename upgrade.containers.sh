45d8d9602a65ba637068fc03b502d33ea90a1604
Phil Jerkins <Phil@TheJerkins.net>
2025-11-29 12:11:05 -0600

    feature: Initial commit

---
+++ upgrade.containers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export MYSQL_ROOT_PASSWORD=$(cat ../secrets/mysql.root.password)
+export OPENWEBUI_OAUTH_CLIENT_ID=$(cat ../secrets/openwebui.oauth.client_id)
+export OPENWEBUI_OAUTH_CLIENT_SECRET=$(cat ../secrets/openwebui.oauth.client_secret)
+export AWS_SES_SMTP_USER_USERNAME=$(cat ../secrets/aws.ses.smtp_user.username)
+export AWS_SES_SMTP_USER_PASSWORD=$(cat ../secrets/aws.ses.smtp_user.password)
+
+docker compose -f ghost.home.compose pull
+docker compose -f ghost.phil.compose pull
+docker compose -f mysql.compose pull
+docker compose -f n8n.compose pull
+docker compose -f nextcloud.compose pull
+docker compose -f nginx.compose pull
+docker compose -f openwebui.compose pull
+
+docker compose -f ghost.home.compose -p home up -d
+docker compose -f ghost.phil.compose -p phil up -d
+docker compose -f mysql.compose -p mysql up -d
+docker compose -f n8n.compose -p n8n up -d
+docker compose -f nextcloud.compose -p nextcloud up -d
+docker compose -f nginx.compose -p nginx up -d
+docker compose -f openwebui.compose -p oi up -d
